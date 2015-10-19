a104b211e49544c1013aa701839101f7eb371735
Andy Feng <andyfengc@outlook.com>
2015-10-19 16:09:05 -0400

    first commit

---
+++ SerilogTutorial/HelloSerilog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Serilog;
+
+namespace SerilogTutorial
+{
+    public class HelloSerilog
+    {
+        public static void Main(string[] args)
+        {
+            // Create Logger
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.Console()
+                    .CreateLogger();
+            // Output logs
+            Log.Information("Hello, Serilog!");
+        }
+    }
+}
