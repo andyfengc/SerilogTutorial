4e86cddbf27b756c91d973701b0f27871f4be40c
Andy Feng <andyfengc@gmail.com>
2015-10-20 00:05:21 -0400

    add basic logger add customized logger

---
+++ SerilogTutorial/BasicLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace SerilogTutorial
+{
+    public class BasicLogger
+    {
+        public static void Run()
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.RollingFile(@"D:\Log-{Date}.txt")
+                .CreateLogger();
+            var appointment =
+                new { Id = 1, Subject = "Meeting of database migration", Timestamp = new DateTime(2015, 3, 12) };
+            logger.Information("An appointment is booked successfully: {@appountment}", appointment);
+            logger.Error("Failed to book an appointment: {@appountment}", appointment);
+        }
+    }
+}
--- SerilogTutorial/HelloSerilog.cs
+++ SerilogTutorial/HelloSerilog.cs
@@ -7,12 +7,46 @@ namespace SerilogTutorial
     {
         public static void Main(string[] args)
         {
+            //Hello();
+            // ParameterizedLog();
+            //TemplateLog();
+
+            BasicLogger.Run();
+
+            //CustomizedLogger.Se tLevel();
+        }
+
+        public static void Hello()
+        {
             // Create Logger
-            Log.Logger = new LoggerConfiguration()
+            var logger = new LoggerConfiguration()
                     .WriteTo.Console()
                     .CreateLogger();
             // Output logs
-            Log.Information("Hello, Serilog!");
+            logger.Information("Hello, Serilog!");
+        }
+
+        public static void ParameterizedLog()
+        {
+            // Create Logger
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            // write structured data
+            logger.Information("Processed {Number} records in {Time} ms", 500, 120);
+        }
+
+        public static void TemplateLog()
+        {
+            // Create Logger
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            // prepare data
+            var order = new {Id = 12, Total = 128.50, CustomerId = 72};
+            var customer = new {Id = 72, Name = "John Smith"};
+            // write log message
+            logger.Information("New orders {OrderId} by {Customer}", order.Id, customer);
         }
     }
 }
